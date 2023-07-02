7659fc1b3fa67658b1c978223624744b126dbf4e
tizenkethur <vivientalos29@gmail.com>
2023-07-02 14:11:43 +0200

    routing changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,33 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'fooldal', pathMatch: 'full' },
   {
-    path: 'fooldal',
+    path: '',
     loadChildren: () =>
       import('src/app/features/main/main.module').then((m) => m.MainModule),
-  },
-
-  {
-    path: 'sorszamoszto-automata',
-    loadChildren: () =>
-      import(
-        'src/app/features/serial-number-machine/serial-number-machine.module'
-      ).then((m) => m.SerialNumberMachineModule),
-  },
-  {
-    path: 'kozponti-kijelzo',
-    loadChildren: () =>
-      import('src/app/features/center-screen/center-screen.module').then(
-        (m) => m.CenterScreenModule
-      ),
-  },
-  {
-    path: 'behivo-felulet',
-    loadChildren: () =>
-      import(
-        'src/app/features/calling-controller/calling-controller.module'
-      ).then((m) => m.CallingControllerModule),
   },
 ];
 
--- src/app/features/main/main-routing.module.ts
+++ src/app/features/main/main-routing.module.ts
@@ -6,6 +6,29 @@ const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: 'sorszamoszto-automata',
+        loadChildren: () =>
+          import(
+            'src/app/features/serial-number-machine/serial-number-machine.module'
+          ).then((m) => m.SerialNumberMachineModule),
+      },
+      {
+        path: 'kozponti-kijelzo',
+        loadChildren: () =>
+          import('src/app/features/center-screen/center-screen.module').then(
+            (m) => m.CenterScreenModule
+          ),
+      },
+      {
+        path: 'behivo-felulet',
+        loadChildren: () =>
+          import(
+            'src/app/features/calling-controller/calling-controller.module'
+          ).then((m) => m.CallingControllerModule),
+      },
+    ],
   },
 ];
 
