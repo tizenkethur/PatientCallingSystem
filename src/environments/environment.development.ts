a20d663d38f7263f1c53224d7ddfe15ab45fb6f7
tizenkethur <vivientalos29@gmail.com>
2023-07-02 15:54:32 +0200

    design for serial number component changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://4b32c64d-f173-4598-81ec-f4d355407383.mock.pstmn.io',
+  apiUrl: 'https://6af4b463-b523-4b5a-810f-3cd46b3d54ba.mock.pstmn.io',
   // apiUrl: 'https://felveteli.tigra.hu/feladat/frontend1/api',
 };
