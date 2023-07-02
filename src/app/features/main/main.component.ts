e4e221788f749a509b041126a4be1abae5550433
tizenkethur <vivientalos29@gmail.com>
2023-07-02 14:08:02 +0200

    animation added to main page

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,9 +1,17 @@
-import { SerialNumberMachineComponent } from './../serial-number-machine/serial-number-machine.component';
+import { style, transition, trigger, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(800, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class MainComponent {}
