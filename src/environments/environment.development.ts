bef79431bcce304f2575b7d06e413ef4462b75df
tizenkethur <vivientalos29@gmail.com>
2023-07-03 14:42:41 +0200

    apiUrl changed to the official backend link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://08a26200-15b4-4272-a06a-15e73f40c214.mock.pstmn.io',
-  // apiUrl: 'https://felveteli.tigra.hu/feladat/frontend1/api',
+  apiUrl: 'https://felveteli.tigra.hu/feladat/frontend1/api',
 };
